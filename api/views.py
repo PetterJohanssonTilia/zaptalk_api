064fe580e11cb7ec24c89201612740fd949db7ed
petter johansson <petter.j@outlook.com>
2024-09-09 09:51:12 +0000

    Import: Import pagination in views.py

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.pagination import PageNumberPagination
 from django.contrib.auth.models import User
 from .models import Movie, UserProfile, Like, Comment, Follow
 from .serializers import MovieSerializer, UserSerializer, UserProfileSerializer, LikeSerializer, CommentSerializer, FollowSerializer
